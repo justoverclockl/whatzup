72e17ccc7cefdddd5411a632918b8702fff43857
Marco Colia <marco.colia@fiftydeg.com>
2024-06-20 09:19:56 +0200

    added localAuth

--- src/authStrategies/LocalAuth.ts
+++ src/authStrategies/LocalAuth.ts
@@ -1,3 +1,12 @@
-export class LocalAuth {
+import { BaseAuthStrategy } from './BaseAuthStrategy'
 
+export class LocalAuth extends BaseAuthStrategy {
+    constructor() {
+        super()
+    }
+
+    async beforeBrowserInitialized() {}
+    async afterBrowserInitialized() {}
+    async destroy() {}
+    async logout() {}
 }
